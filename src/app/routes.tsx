1369a715e301aaa0856fc6961a58d86386d81e59
blaiseludvig <blaiseludvig@gmail.com>
2023-04-18 15:06:28 +0200

    add routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,9 +1,19 @@
 import { Route } from '@tanstack/react-location';
+
+import AppView from './app-view/app-view';
 import FrontPage from './front-page/front-page';
+import NotFoundPage from './not-found-page/not-found-page';
 
 export const routes: Route[] = [
   {
     path: '/',
     element: <FrontPage />,
+  },
+  {
+    path: '/app',
+    element: <AppView />,
+  },
+  {
+    element: <NotFoundPage />,
   },
 ];
