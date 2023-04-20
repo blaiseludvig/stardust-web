19ff7252d0da8c5e2830d1df94c8b048be10c08e
blaiseludvig <blaiseludvig@gmail.com>
2023-04-21 01:57:39 +0200

    refactor modal-frame

--- src/app/util/modal-frame/modal-frame.tsx
+++ src/app/util/modal-frame/modal-frame.tsx
@@ -1,68 +1,51 @@
 import { useFirstMountState } from '@react-hookz/web';
-import { ModalInterface, ModalOptions } from 'flowbite';
-import { Modal as FlowbiteModal } from 'flowbite';
-import {
-  forwardRef,
-  useImperativeHandle,
-  useLayoutEffect,
-  useRef,
-} from 'react';
+import { useNavigate } from '@tanstack/react-location';
+import classNames from 'classnames';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import { useCloseModal } from 'src/app/hooks/useCloseModal';
+import { LocationGenerics } from 'src/app/routes';
 
-import styles from './modal-frame.module.scss';
+export interface ModalFrameProps {
+  hidden: boolean;
+  onShow?: () => void;
+  onHide?: () => void;
+}
 
-export type ModalFrameHandle = {
-  show: () => void;
-  hide: () => void;
-  toggle: () => void;
-  setVisible(isVisible: boolean): void;
-  setHidden(isHidden: boolean): void;
-};
+function ModalFrame(props: React.PropsWithChildren<ModalFrameProps>) {
+  const { hidden } = props;
 
-/* eslint-disable-next-line */
-export interface ModalFrameProps extends ModalOptions {}
-
-export const ModalFrame = forwardRef<
-  ModalFrameHandle,
-  React.PropsWithChildren<ModalFrameProps>
->((props: React.PropsWithChildren<ModalFrameProps>, ref) => {
-  useImperativeHandle(ref, () => {
-    return {
-      show: () => flowbiteModalHandle.current?.show(),
-      hide: () => flowbiteModalHandle.current?.hide(),
-      toggle: () => flowbiteModalHandle.current?.toggle(),
-      setVisible(isVisible: boolean) {
-        return isVisible ? this.show() : this.hide();
-      },
-      setHidden(isHidden: boolean) {
-        return isHidden ? this.hide() : this.show();
-      },
-    } satisfies ModalFrameHandle;
-  });
+  const navigate = useNavigate<LocationGenerics>();
+  const closeModal = useCloseModal();
 
   const isFirstMount = useFirstMountState();
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const flowbiteModalHandle = useRef<ModalInterface | null>(null);
 
-  useLayoutEffect(() => {
-    if (!isFirstMount && !flowbiteModalHandle.current) {
-      flowbiteModalHandle.current = new FlowbiteModal(containerRef.current, {
-        ...props,
-      });
+  useEffect(() => {
+    if (!isFirstMount) {
+      props.hidden ? props.onHide?.() : props.onShow?.();
     }
-  });
+    // only needs to run when showing / hiding the modal, so we can disable this
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hidden]);
 
   return createPortal(
     <div
-      ref={containerRef}
-      tabIndex={-1}
-      aria-hidden="true"
-      className="absolute top-0 left-0 right-0 z-50 hidden h-[calc(100%-1rem)] w-full overflow-y-auto overflow-x-hidden p-4 md:inset-0 md:h-full"
+      className={classNames(
+        props.hidden && 'hidden',
+        'fixed inset-0 z-[100] bg-gray-900 bg-opacity-50 dark:bg-opacity-80'
+      )}
     >
-      {props.children}
+      <div
+        onMouseDown={(event) => {
+          if (event.target === event.currentTarget) closeModal();
+        }}
+        className="absolute top-0 left-0 right-0 z-[110] flex h-[calc(100%-1rem)] w-full items-center justify-center overflow-y-auto overflow-x-hidden p-4 md:inset-0 md:h-full"
+      >
+        {props.children}
+      </div>
     </div>,
     document.getElementById('app') as HTMLElement
   );
-});
+}
 
 export default ModalFrame;
