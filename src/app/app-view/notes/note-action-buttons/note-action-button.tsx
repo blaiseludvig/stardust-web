8807c5beec863f1d3685dc8fabeea4850ba76996
blaiseludvig <blaiseludvig@gmail.com>
2023-04-29 01:43:46 +0200

    remove unnecessary background color from note-action-button

--- src/app/app-view/notes/note-action-buttons/note-action-button.tsx
+++ src/app/app-view/notes/note-action-buttons/note-action-button.tsx
@@ -16,7 +16,7 @@ function NoteActionButton(props: NoteActionButtonProps) {
         onMouseDown={props.action}
         type="button"
         className={classNames(
-          'inline-flex h-12 w-12 items-center rounded-full border-gray-200 bg-white text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white',
+          'inline-flex h-12 w-12 items-center rounded-full border-gray-200 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 dark:border-gray-600 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white',
           props.className
         )}
       >
