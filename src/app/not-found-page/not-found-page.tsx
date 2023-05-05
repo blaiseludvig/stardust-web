92a420189e36837a109606c2729820f9d36ce94c
blaiseludvig <blaiseludvig@gmail.com>
2023-05-05 18:28:38 +0200

    remove unused .scss files

--- src/app/not-found-page/not-found-page.tsx
+++ src/app/not-found-page/not-found-page.tsx
@@ -1,13 +1,11 @@
 import { Link } from '@tanstack/react-location';
 
-import styles from './not-found-page.module.scss';
-
 /* eslint-disable-next-line */
 export interface NotFoundPageProps {}
 
 export function NotFoundPage(props: NotFoundPageProps) {
   return (
-    <div className={styles['container']}>
+    <div>
       <h1>Welcome to NotFoundPage!</h1>
       <Link to={'/'}>Back to front page</Link>
     </div>
