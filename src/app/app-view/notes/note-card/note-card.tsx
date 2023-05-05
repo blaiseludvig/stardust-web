56bd68d588e763dc9cf5af5605924e68efeca5fe
blaiseludvig <blaiseludvig@gmail.com>
2023-05-05 21:14:03 +0200

    add text truncation to note-card

--- src/app/app-view/notes/note-card/note-card.tsx
+++ src/app/app-view/notes/note-card/note-card.tsx
@@ -46,33 +46,60 @@ export function NoteCard(props: NoteCardProps) {
       onMouseOver={() => toggleShowActionButtons(true)}
       onMouseOut={() => toggleShowActionButtons(false)}
     >
-      <ReactTextareaAutosize
-        ref={titleRef}
-        onSelect={(event) => {
-          if (titleRef.current?.selectionStart) {
-            editModalCursor.setCursor(
-              'title',
-              titleRef.current?.selectionStart
-            );
-          }
-        }}
-        className="mb-2 w-full cursor-default resize-none border-none bg-transparent p-0 text-2xl font-bold tracking-tight text-white caret-transparent selection:bg-transparent"
-        value={title}
-      ></ReactTextareaAutosize>
+      <div
+        // I want o be able to use line-clamp with the ellipsis, and the
+        // only way I found that lets me keep the feature where the
+        // caret is transfered to the modal is to overlay an invisible
+        // <textarea> with the exact same styling on top of a <p>
+        // element. Interesting hack.
+        className="relative isolate"
+      >
+        <p className="line-clamp-[3] mb-2 w-full cursor-default resize-none whitespace-pre-wrap border-none bg-transparent p-0 text-2xl font-bold tracking-tight text-white caret-transparent selection:bg-transparent">
+          {title}
+        </p>
 
-      <ReactTextareaAutosize
-        ref={contentRef}
-        onSelect={(event) => {
-          if (contentRef.current?.selectionStart) {
-            editModalCursor.setCursor(
-              'content',
-              contentRef.current?.selectionStart
-            );
-          }
-        }}
-        className="w-full cursor-default resize-none border-none bg-transparent p-0 font-normal text-gray-400 caret-transparent selection:bg-transparent"
-        value={content}
-      ></ReactTextareaAutosize>
+        <ReactTextareaAutosize
+          ref={titleRef}
+          // set to same value as the line-clamp
+          maxRows={3}
+          // prevent dragging the invisible text
+          onDragStart={(event) => event.preventDefault()}
+          onSelect={(event) => {
+            if (titleRef.current?.selectionStart) {
+              editModalCursor.setCursor(
+                'title',
+                titleRef.current?.selectionStart
+              );
+            }
+          }}
+          className="absolute top-0 z-[1] mb-2 w-full cursor-default resize-none overflow-hidden border-none p-0 text-2xl font-bold tracking-tight caret-transparent opacity-0 selection:bg-transparent"
+          value={title}
+        ></ReactTextareaAutosize>
+      </div>
+
+      <div className="relative isolate">
+        <p className="line-clamp-[8] w-full cursor-default resize-none whitespace-pre-wrap border-none bg-transparent p-0 font-normal text-gray-400 caret-transparent selection:bg-transparent">
+          {content}
+        </p>
+
+        <ReactTextareaAutosize
+          ref={contentRef}
+          // set to same value as the line-clamp
+          maxRows={8}
+          // prevent dragging the invisible text
+          onDragStart={(event) => event.preventDefault()}
+          onSelect={(event) => {
+            if (contentRef.current?.selectionStart) {
+              editModalCursor.setCursor(
+                'content',
+                contentRef.current?.selectionStart
+              );
+            }
+          }}
+          className="absolute top-0 z-[1] w-full cursor-default resize-none overflow-hidden border-none p-0 font-normal caret-transparent opacity-0 selection:bg-transparent"
+          value={content}
+        ></ReactTextareaAutosize>
+      </div>
 
       <NoteActionButtons hidden={!showActionButtons} noteData={props.data} />
     </div>
