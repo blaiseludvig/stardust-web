8ed4c81ca8081aa209e3d06f860a04b17afc47ab
blaiseludvig <blaiseludvig@gmail.com>
2023-08-17 01:04:40 +0200

    remove all dark: modifiers from notes-dial

--- src/app/app-view/notes/notes-dial/notes-dial.tsx
+++ src/app/app-view/notes/notes-dial/notes-dial.tsx
@@ -24,7 +24,7 @@ export function NotesDial(props: NotesDialProps) {
       pinnable
       className={clsx(
         !isAuthenticated && 'invisible',
-        'fixed right-6 bottom-6'
+        'fixed bottom-6 right-6'
       )}
       triggerButton={
         <>
@@ -33,7 +33,7 @@ export function NotesDial(props: NotesDialProps) {
               navigate({ search: (old) => ({ ...old, modal: 'new-note' }) })
             }
             type="button"
-            className="group flex h-14 w-14 items-center justify-center rounded-full bg-blue-700 text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="group flex h-14 w-14 items-center justify-center rounded-full bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800"
           >
             <PlusIcon className="h-6 stroke-[3px] text-white transition-[height] group-hover:h-8" />
           </button>
