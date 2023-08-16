867d85ff3cfa900d535327e39b60f1ff351ec1ee
blaiseludvig <blaiseludvig@gmail.com>
2023-08-17 00:52:32 +0200

    remove all dark: modifiers from zero-notes

--- src/app/app-view/notes/note-views/all-notes/zero-notes.tsx
+++ src/app/app-view/notes/note-views/all-notes/zero-notes.tsx
@@ -12,7 +12,7 @@ export function ZeroNotes() {
           onMouseDown={() =>
             navigate({ search: (old) => ({ ...old, modal: 'new-note' }) })
           }
-          className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800"
         >
           Create note
         </button>
